653f4013867848f15da4414aca30a9fbb1acdcf1
green-21 <133c96d@naver.com>
2023-05-22 18:19:56 +0900

    PlaceDetailView 디자인 개선

--- src/PlaceDetailView.js
+++ src/PlaceDetailView.js
@@ -1,8 +1,26 @@
 'use strict'
 
 import React, { Component } from 'react';
-import { StyleSheet, View, Button, TextInput, Modal, Text, Pressable } from 'react-native';
+import { 
+    StyleSheet, 
+    View, 
+    Button, 
+    Modal, 
+    Text, 
+    Pressable, 
+    Linking, 
+    Image,
+    ScrollView, 
+} from 'react-native';
 
+const Icons = {
+    "location": require('../res/icon/location.png'),
+    "phone-full": require('../res/icon/phone-full.png'),
+    "phone": require('../res/icon/phone.png'),
+    "star-full": require('../res/icon/star-full.png'),
+    "star": require('../res/icon/star.png'),
+    "clock": require('../res/icon/clock.png'),
+};
 
 export default class PlaceDetailView extends Component {
     constructor(props) {
@@ -22,14 +40,38 @@ export default class PlaceDetailView extends Component {
                     onPress={closeModal}
                 />
                 <View style={styles.modal}>
-                    <Text>{place.name}</Text>
-
+                    <ScrollView>
+                    <Text style={styles.titleText}>
+                        {place.name}
+                    </Text>
+                    <PlaceText res={Icons['star-full']} value={`${place.rating} / 5.0`} />
+                    <PlaceText res={Icons['location']} value={place.address} />
+                    <PlaceText res={Icons['phone-full']} value={place.number} />
+                    <PlaceText res={Icons['clock']} value={place.open_hour} />
                     <Button
-                        title="Hide Modal"
-                        onPress={closeModal}
+                        title={"웹 사이트"}
+                        onPress={() => { Linking.openURL(place.site) }}
                     />
+                    </ScrollView>
                 </View>
             </Modal>
+        )
+    }
+}
+
+class PlaceText extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const { res, value } = this.props;
+        return (
+            <View style={styles.infoView}>
+                <View style={styles.infoName}>
+                    <Image source={res} style={styles.infoNameIcon} />
+                </View>
+                <Text style={styles.infoDetailText}>{value}</Text>
+            </View>
         )
     }
 }
@@ -41,19 +83,43 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: 'rgba(0,0,0,0.1)',
     },
+
     paddingSpace: {
         flex: 2,
         backgroundColor: 'transparent',
     },
+
     modal: {
         flex: 3,
         borderTopLeftRadius: 10,
         borderTopRightRadius: 10,
         marginLeft: '4%',
         marginRight: '4%',
-        paddingTop: 50,
+        paddingTop: 30,
         paddingLeft: 15,
         paddingRight: 15,
         backgroundColor: '#fff',
+    },
+
+    titleText: {
+        fontSize: 35,
+        fontWeight: "bold",
+        marginBottom: 28,
+    },
+    infoView: {
+        flexDirection: "row",
+        marginBottom: 10,
+        borderBottomColor: "#eee"
+    },
+    infoName: {
+        flex:1,
+    },
+    infoNameIcon: {
+        width: 24,
+        height: 24
+    },
+    infoDetailText: {
+        flex: 7,
+        fontSize: 16,
     },
 })
